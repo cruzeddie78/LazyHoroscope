bc90d9deab06c1726a2892f5befdffd1238a5c40
cruzeddie78 <ecruz19@mail.csuchico.edu>
2018-09-19 17:55:36 -0700

    I created a value to my two strings and I got the vlaues of 0 for both of them so it can output the users initials as their nickname. Then I assigned an integer a variable so it can print out the users luckk number to be the length of their first name using a length function.

--- main.cpp
+++ main.cpp
@@ -8,17 +8,19 @@ int main()
 {
 string first_name;
 string last_name;
+int lucky;
 
 
   cout<<" What is your first name?";
   cin>>first_name;
   cout<<"What is your last name?";
   cin>>last_name;
+  cout<<"Welcome, "<< first_name[0] <<last_name[0]<<" here is your fortune"<<endl; 
 
-
-
-  //tell fortune
-
+  
+  
+  lucky=first_name.length();
+  cout<<"Your lucky number is, "<<lucky<<endl;
   return 0;
 }
 
